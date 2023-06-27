0b42523d06d64856d75907ede640c019c34ddfa8
hsmkazemi <hsm.kazemi@gmail.com>
2023-06-27 22:30:56 +0200

    conversion class is implemented

---
+++ app/src/main/java/com/postfixcalculator/Expression.kt
@@ -0,0 +1,68 @@
+package com.postfixcalculator
+
+import java.util.Stack
+
+class Expression(var infixExpression: MutableList<String>) {
+    private fun infixToPostfix(): String {
+        var result = ""
+        val stack = Stack<String>()
+        for (element in infixExpression) {
+            if (element.all { it.isDigit() } || element.any { it == '.' }) {
+                result += "$element "
+            } else if (element == "(") {
+                stack.push(element)
+            } else if (element == ")") {
+                do {
+                    result += "${stack.pop()} "
+                } while (stack.peek() != "(" && stack.isNotEmpty())
+            } else {
+                while (stack.isNotEmpty() && precedence(stack.peek()) >= precedence(element)) {
+                    result += "${stack.pop()} "
+                }
+            }
+        }
+        while (stack.isNotEmpty()) {
+            result += "${stack.pop()} "
+        }
+        return result
+    }
+
+    private fun precedence(operator: String): Int {
+        return when (operator) {
+            "*", "/" -> 2
+            "-", "+" -> 1
+            else -> -1
+
+        }
+    }
+
+    fun evaluateExpression(postFix: String): Number {
+        val stack = Stack<Double>()
+        var i = 0
+        while (i < postFix.length) {
+            if (postFix[i] == ' ') {
+                i++
+                continue
+            } else if (Character.isDigit(postFix[i])) {
+                var number = ""
+                while (Character.isDigit(postFix[i]) || postFix[i] == '.') {
+                    number += postFix[i]
+                    i++
+                }
+                stack.push(number.toDouble())
+            } else {
+                val x = stack.pop()
+                val y = stack.pop()
+                when (postFix[i]) {
+                    '*' -> stack.push(x * y)
+                    '/' -> stack.push(y / x)
+                    '+' -> stack.push(x + y)
+                    '-' -> stack.push(y - x)
+                }
+            }
+            i++
+        }
+        return if (stack.peek() / stack.peek().toInt() == 1.0) stack.peek()
+            .toInt() else stack.peek()
+    }
+}
